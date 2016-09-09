8ef32b06bdcee90648f29cc08879c786ccccd2c8
jaruwan57 <jaruwan.paru57@sskru.ac.th>
2016-09-09 14:29:13 +0700

    new Project

---
+++ Hello_OpenSource/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_OpenSource
+{
+    class Hello_
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello jaruwan Paruearang!");
+            
+            // Keep the console window open in debug mode.
+            Console.WriteLine("press any key to exit.");
+            Console.ReadKey();
+        }
+
+        }
+    }
